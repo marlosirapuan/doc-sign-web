21238767ef4ca37d0bca0ac8826fe046eb8b1949
Marlos <marlos.irapuan@gmail.com>
2025-04-27 09:00:04 -0300

    feat: add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+import type { PluginOptions } from 'babel-plugin-react-compiler'
+
+const ReactCompilerConfig = {} as PluginOptions
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]]
+      }
+    }),
+    tsConfigPaths()
+  ]
 })
