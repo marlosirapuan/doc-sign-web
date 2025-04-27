418798c96693ada80a2cec83af95213c48436342
Marlos <marlos.irapuan@gmail.com>
2025-04-27 07:41:21 -0300

    feat: add axios and styling login page

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 
 import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
 
 import { App } from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider>
+      <Notifications position="top-right" />
       <App />
     </MantineProvider>
   </StrictMode>
--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,9 +1,100 @@
-import { Box, Text } from '@mantine/core'
+import { useState } from 'react'
+
+import { Box, Button, Center, Container, Paper, PasswordInput, Stack, Text, TextInput, Title } from '@mantine/core'
+import { useNavigate } from 'react-router'
+
+import { useForm } from '@mantine/form'
+import { showNotification } from '@mantine/notifications'
+
+import { api } from '../libs/axios'
+
+import { ThemeSwitch } from '../components/theme-switch'
+
+import { useAuth } from '../contexts/auth-context'
 
 export const LoginPage = () => {
+  //
+  // Context
+  //
+  const auth = useAuth()
+
+  //
+  // Hooks
+  //
+  const navigate = useNavigate()
+
+  //
+  // State
+  //
+  const [isProcessing, setIsProcessing] = useState(false)
+
+  //
+  // Forms
+  //
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email')
+    }
+  })
+
+  //
+  // Functions
+  //
+  const handleSubmit = async (values: typeof form.values) => {
+    setIsProcessing(true)
+
+    try {
+      const res = await api.post('http://localhost:3000/login', {
+        email: values.email,
+        password: values.password
+      })
+
+      const token = res.data.token
+
+      auth?.login(token)
+
+      navigate('/')
+    } catch (error) {
+      console.error(error)
+
+      showNotification({
+        title: 'Error',
+        message: 'Invalid email or password',
+        color: 'red'
+      })
+    } finally {
+      setIsProcessing(false)
+    }
+  }
+
   return (
-    <Box>
-      <Text>todo</Text>
-    </Box>
+    <Container size={420} my={40}>
+      <Box component="form" onSubmit={form.onSubmit(handleSubmit)}>
+        <Title ta="center">eSignature</Title>
+
+        <Text c="dimmed" size="sm" ta="center" mt={5}>
+          Small web app for signing documents
+        </Text>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <Stack>
+            <TextInput label="Email" placeholder="user1@example.com" required {...form.getInputProps('email')} />
+            <PasswordInput label="Password" placeholder="password123" required {...form.getInputProps('password')} />
+          </Stack>
+
+          <Button fullWidth mt="xl" type="submit" loading={isProcessing}>
+            Sign in
+          </Button>
+        </Paper>
+
+        <Center mt="xs">
+          <ThemeSwitch />
+        </Center>
+      </Box>
+    </Container>
   )
 }
