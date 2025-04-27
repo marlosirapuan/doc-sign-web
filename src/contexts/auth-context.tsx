b691117b95ffe28ffca56ac4f577bdf7e9eefce7
Marlos <marlos.irapuan@gmail.com>
2025-04-27 06:45:18 -0300

    feat: add auth context

---
+++ src/contexts/auth-context.tsx
@@ -0,0 +1,35 @@
+import { type ReactNode, createContext, useEffect, useState } from 'react'
+
+interface AuthContextType {
+  token: string | null
+  login: (token: string) => void
+  logout: () => void
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null)
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState<string | null>(null)
+
+  useEffect(() => {
+    const savedToken = localStorage.getItem('token')
+
+    if (savedToken) {
+      setToken(savedToken)
+    }
+  }, [])
+
+  const login = (token: string) => {
+    localStorage.setItem('token', token)
+
+    setToken(token)
+  }
+
+  const logout = () => {
+    localStorage.removeItem('token')
+
+    setToken(null)
+  }
+
+  return <AuthContext.Provider value={{ token, login, logout }}>{children}</AuthContext.Provider>
+}
