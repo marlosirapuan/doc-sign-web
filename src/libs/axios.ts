99c1b159299bf86efa48a0ca7ff8a0fccee70323
Marlos <marlos.irapuan@gmail.com>
2025-04-27 08:25:39 -0300

    refactor: axios with token

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
+const token = localStorage.getItem('token')
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://localhost:3000',
+  headers: {
+    Authorization: token ? `Bearer ${token}` : ''
+  }
 })
